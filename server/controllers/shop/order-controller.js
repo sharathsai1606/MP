baf6bcba0a909157d9889a5b634722c2cc6803ac
Sharath Sai <sharathsai578@gmail.com>
2024-12-12 01:22:33 +0530

    final

--- server/controllers/shop/order-controller.js
+++ server/controllers/shop/order-controller.js
@@ -26,8 +26,8 @@ const createOrder = async (req, res) => {
         payment_method: "paypal",
       },
       redirect_urls: {
-        return_url: "http://localhost:5173/shop/paypal-return",
-        cancel_url: "http://localhost:5173/shop/paypal-cancel",
+        return_url: "https://mp-client-s6ow.onrender.com/shop/paypal-return",
+        cancel_url: "https://mp-client-s6ow.onrender.com/shop/paypal-cancel",
       },
       transactions: [
         {
